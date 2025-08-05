113fed5a07869e76e203075f6bd98fc8ae93a62e
TAky5566 <we98e302@gmail.com>
2025-08-05 05:10:02 +0300

    ...

--- todo/main.js
+++ todo/main.js
@@ -1,64 +1,66 @@
 let In = document.querySelector("#input_task");
 let btn = document.querySelector(`#add_task`);
-let tasks = JSON.parse(localStorage.getItem("tasks"))||[];
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-window.onload= _ => In.focus();
+window.onload = (_) => In.focus();
 
-tasks.forEach(task => {
-    console.log(tasks);
+tasks.forEach((task) => {
+  console.log(tasks);
 
-    creatElement(task.title , task.id , task.statue);
+  creatElement(task.title, task.id, task.statue);
 });
 
-btn.addEventListener("click",()=>{
-if(In.value){
+btn.addEventListener("click", () => {
+  if (In.value) {
     let newtask = {
-        id : Date.now() ,
-        title:In.value , 
-        statue:"0"
+      id: Date.now(),
+      title: In.value,
+      statue: "0",
     };
     tasks.push(newtask);
-    localStorage.setItem("tasks",JSON.stringify(tasks))
-    creatElement(In.value , newtask.id);
-}
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    creatElement(In.value, newtask.id);
+  }
 });
 
-function creatElement(text , id , state = null) {
-let ticket = document.createElement("div");
-ticket.classList.add("task_container");
-if(state == true)ticket.classList.add("done");
- 
-ticket.innerHTML=`
-<input type="checkbox"  id="state"${(state==true)?"checked":""} onclick="changestate(this.parentElement)">
+function creatElement(text, id, state = null) {
+  let ticket = document.createElement("div");
+  ticket.classList.add("task_container");
+  if (state == true) ticket.classList.add("done");
+
+  ticket.innerHTML = `
+<input type="checkbox"  id="state"${
+    state == true ? "checked" : ""
+  } onclick="changestate(this.parentElement)">
 <p class="task_content" data-id="${id}"> ${text}</p>
 <input type="button" value="delete" id="delete_btn" onclick="erase(this.parentElement)">
 `;
 
-    document.querySelector(".tasks").appendChild(ticket);
+  document.querySelector(".tasks").appendChild(ticket);
 }
 
-function erase(x){
-    let id = x.children[1].dataset.id;
-    tasks = tasks.filter( t => t.id != id );
-    localStorage.setItem("tasks",{});
-    localStorage.setItem("tasks",JSON.stringify(tasks));
-    console.log(id)
-    x.remove();
+function erase(x) {
+  let id = x.children[1].dataset.id;
+  tasks = tasks.filter((t) => t.id != id);
+  localStorage.setItem("tasks", {});
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  console.log(id);
+  x.remove();
 }
-function changestate(x){
-    x.classList.toggle("done");
-    let state = x.children[0].checked;
-    let input = x.children[1];
-    let id= input.dataset.id;
-    let newtask = {
-        id :id ,
-        title: input.innerHTML, 
-        statue:state
-    };
+function changestate(x) {
+  x.classList.toggle("done");
+  let state = x.children[0].checked;
+  let input = x.children[1];
+  let id = input.dataset.id;
+  let newtask = {
+    id: id,
+    title: input.innerText,
+    statue: state,
+  };
 
-    tasks = tasks.filter( t => t.id != id );
-    tasks.push(newtask);
-    console.log(tasks);
-    localStorage.setItem("tasks",{});
-    localStorage.setItem("tasks",JSON.stringify(tasks));
+  tasks = tasks.filter((t) => t.id != id);
+  tasks.push(newtask);
+  console.log(tasks);
+  localStorage.setItem("tasks", {});
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
